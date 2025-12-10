57d60a4fdfc57f9175b8f8ebab51d1c31fe98c2b
akhil joy <akhiljoy312@gmail.com>
2025-12-10 13:18:11 +0530

    fix: changed vite config for ngrok testing

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+    allowedHosts: ["glaiked-harry-unphotographed.ngrok-free.dev"],
     proxy: {
       "/api": {
         target: "http://localhost:3000",
